cd9ba54f32933d7458bf92075934b50de2504f56
Fury_WK <64100351+Worrakorn26234@users.noreply.github.com>
2021-05-20 01:40:03 +0700

    Add files via upload

---
+++ FTX_rebalance_notify.py
@@ -0,0 +1,59 @@
+from ccxt.ftx import ftx
+from songline import Sendline
+import ccxt
+import pandas as pd
+import json
+import time
+token = 'put your line token'
+messenger = Sendline(token)
+
+############################ Setting #############################################
+
+API_Key = 'put your api key' 
+API_secret = 'put your api secret'
+Subaccount = 'put your subaccount name'
+Pair = "SRM/USD" 
+Token_name = "SRM"
+Rebalance_value = 1000  ## put your rebalance value ##
+
+
+############################### เรียกใช้งาน API #############################################
+FTX = ccxt.ftx({
+    'apiKey' : API_Key ,
+    'secret' : API_secret , 
+    'headers': {'FTX-SUBACCOUNT': 'put your subaccount name'}, 
+    'enableRateLimit': True
+})
+
+print(messenger.sendtext('Exchange = ' + str(FTX)))      ## Network checking / Line notify ##
+
+################################# Price of SRM #############################################
+
+SRM_value = json.dumps(FTX.fetch_ticker('SRM/USD'))
+Price_SRM = json.loads(SRM_value)
+print(messenger.sendtext('SRM = ' + str(Price_SRM['last'])))     ## Line notify ##
+
+############################### Balance #######################################
+
+balance = FTX.fetch_balance()
+
+A = balance['total']['SRM'] * Price_SRM['last']  ## SRM value * Price = net balance ##
+ 
+######### i set this bot to rebalance every 30 min for 48 round (24 hr). ###########
+
+if A < Rebalance_value:
+    amount = Rebalance_value - A
+    for i in range(0,48):                                   ## 48 round (24 hr.)/u can change to what u want ##
+        print(messenger.sendtext('Buy ' + str(amount)))      
+        time.sleep(1800)                                    ## 1800 sec./u can change to what u want ## 
+
+elif A > Rebalance_value:
+    amount = A - Rebalance_value
+    for i in range(0,48):
+        print(messenger.sendtext('Sell ' + str(amount)))
+        time.sleep(1800)
+        
+else:
+    for i in range(0,48):
+        print(messenger.sendtext('Do notihng'))
+        time.sleep(1800)
